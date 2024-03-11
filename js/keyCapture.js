0258a4e7f5a3c357d26577695e5fc7904bf565e5
TheBestMrand <thebestmrand@hotmail.com>
2024-03-11 21:24:34 +0200

    Add publish blazor WASM

---
+++ js/keyCapture.js
@@ -0,0 +1,9 @@
+﻿window.keyCapture = {
+    registerKeyEvents: function (dotNetReference) {
+        document.addEventListener('keydown', (event) => {
+            if (event.target.tagName !== 'INPUT' && event.target.tagName !== 'TEXTAREA') {
+                dotNetReference.invokeMethodAsync('OnKeyPress', event.key);
+            }
+        });
+    }
+};
